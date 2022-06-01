b1af6ab2f7a2c1ab6cebfd76e107bf104669c017
Courtney Struelens <courtneystruelens@hotmail.com>
2022-06-01 00:25:45 -0700

    added seed data

---
+++ seeds/seed.js
@@ -0,0 +1,31 @@
+const sequelize = require("../config/connection");
+const { User, Blog, Comment } = require("../models");
+
+const userData = require("./userData.json");
+const blogData = require("./blogData.json");
+const commentData = require("./commentData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const blogs = await Blog.bulkCreate(blogData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const comments = await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  console.log(users, blogs, comments);
+
+  process.exit(0);
+};
+
+seedDatabase();
